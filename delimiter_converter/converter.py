0191f08ed605459a3609cac07a53eea5070bcfb7
Joel @ Laptop <joel@joelurw.in>
2022-10-24 12:08:26 +0100

    Added column_create() which right now takes the input for each column name (amount of column names is based off the largest line of values)

--- delimiter_converter/converter.py
+++ delimiter_converter/converter.py
@@ -1,17 +1,36 @@
+
 class Converter():
 
     def __init__(self, args):
+        self.dict = dict()
+        self.column_names = list()
+        self.list = list()
+        self.rowdepth = int()
 
         #Converting the file from IOWrapper to a String.
         def convert():
             file = args['f'].read()
             ln_file = file.splitlines()
+            self.rowdepth = 0
+
             for line in ln_file:
                 line = line.split(",")
                 line = [i.strip(' ') for i in line]
                 line = [i.strip('.') for i in line]
-                print(len(line))
-                return line
+                if self.rowdepth < len(line):
+                    self.rowdepth = len(line)
+
+                self.list.append(line)
+
+            column_create()
+
+        def column_create():
+            for x in (range(self.rowdepth)):
+                try:
+                    i = str(input(f"Input Column Name (Data : {self.list[0][x]}): "))
+                    self.column_names.append(i)
+                except ValueError:
+                    print("Value is not a string!")
 
         if args['json'] is True:
             parsed_file = convert()
